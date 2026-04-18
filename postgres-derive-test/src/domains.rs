8533005253920f40e72b2baf68ecb98c8d66b5af
purple-ic <174754933+purple-ic@users.noreply.github.com>
2026-04-18 05:01:43 +0200

    add schema support in derive
    #[postgres(schema = ...)] can be used to make it so the type only accepts types with the matching schema name. if the attribute is omitted, any schema is allowed (same as before).
    
    fixes #627

--- postgres-derive-test/src/domains.rs
+++ postgres-derive-test/src/domains.rs
@@ -49,6 +49,28 @@ fn name_overrides() {
 }
 
 #[test]
+fn schema_overrides() {
+    #[derive(FromSql, ToSql, Debug, PartialEq)]
+    #[postgres(schema = "custom_schema")]
+    struct SessionId(Vec<u8>);
+
+    let mut conn = Client::connect("user=postgres host=localhost port=5433", NoTls).unwrap();
+    conn.batch_execute(
+        "CREATE SCHEMA IF NOT EXISTS custom_schema; CREATE DOMAIN custom_schema.\"SessionId\" AS bytea CHECK(octet_length(VALUE) = 16);",
+    )
+    .unwrap();
+
+    test_type(
+        &mut conn,
+        "custom_schema.\"SessionId\"",
+        &[(
+            SessionId(b"0123456789abcdef".to_vec()),
+            "'0123456789abcdef'",
+        )],
+    );
+}
+
+#[test]
 fn wrong_name() {
     #[derive(FromSql, ToSql, Debug, PartialEq)]
     struct SessionId(Vec<u8>);
@@ -62,6 +84,25 @@ fn wrong_name() {
 
     let err = conn
         .execute("SELECT $1::session_id", &[&SessionId(vec![])])
+        .unwrap_err();
+    assert!(err.source().unwrap().is::<WrongType>());
+}
+
+#[test]
+fn wrong_schema() {
+    #[derive(FromSql, ToSql, Debug, PartialEq)]
+    #[postgres(schema = "foo")]
+    struct SessionId(Vec<u8>);
+
+    let mut conn = Client::connect("user=postgres host=localhost port=5433", NoTls).unwrap();
+    conn.execute(
+        "CREATE DOMAIN pg_temp.\"SessionId\" AS bytea CHECK(octet_length(VALUE) = 16);",
+        &[],
+    )
+    .unwrap();
+
+    let err = conn
+        .execute("SELECT $1::\"SessionId\"", &[&SessionId(vec![])])
         .unwrap_err();
     assert!(err.source().unwrap().is::<WrongType>());
 }
